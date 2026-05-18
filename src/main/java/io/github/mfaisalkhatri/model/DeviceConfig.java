a99df42b9d53a1190bd07f5f90a6e8c2122c08f6
mfaisalkhatri <mohammadfaisalkhatri@gmail.com>
2026-05-18 18:45:55 +0530

    modelling for framework config complete

--- src/main/java/io/github/mfaisalkhatri/model/DeviceConfig.java
+++ src/main/java/io/github/mfaisalkhatri/model/DeviceConfig.java
@@ -1,4 +1,13 @@
 package io.github.mfaisalkhatri.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class DeviceConfig {
+
+    private String deviceName;
+    private String platformVersion;
+    private String automationName;
 }
