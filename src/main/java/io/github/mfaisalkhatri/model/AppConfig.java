8b47ceb4a73b4814a88db366f4eb7ee35997ed0b
Mohammad Faisal Khatri <mohammadfaisalkhatri@gmail.com>
2026-05-18 18:47:22 +0530

    Merge pull request #5 from mfaisalkhatri/issue-1
    config modelling complete

--- src/main/java/io/github/mfaisalkhatri/model/AppConfig.java
+++ src/main/java/io/github/mfaisalkhatri/model/AppConfig.java
@@ -1,4 +1,12 @@
 package io.github.mfaisalkhatri.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AppConfig {
+    private String appPackage;
+    private String appActivity;
+    private String appPath;
 }
